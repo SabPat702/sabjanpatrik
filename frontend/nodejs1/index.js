62ec825555f8eb6581cef8e85c1c53187b640064
Sabján Patrik <SabPat702@hengersor.hu>
2024-12-05 13:22:18 +0100

    Create index.js

---
+++ frontend/nodejs1/index.js
@@ -0,0 +1,9 @@
+
+// wrapper function
+// function(_dirname, _filename, module, require, exports){
+    console.log(_dirname);
+    console.log(_filename);
+    console.log(module);
+    console.log(require);
+    console.log(exports);
+// }
