a738c642f6df8dad4ea7eb18a27ab3c15eaeca94
Sabján Patrik <SabPat702@hengersor.hu>
2024-12-06 11:01:57 +0100

    szerver létrehozása

---
+++ frontend/nodejs1/calculator.js
@@ -0,0 +1,6 @@
+// function(_dirname, _filename, module, require, exports){
+function add(szam1, szam2){
+    return szam1 + szam2;
+}
+module.exports = add;
+// }
--- frontend/nodejs1/index.js
+++ frontend/nodejs1/index.js
@@ -3,7 +3,15 @@
 // function(_dirname, _filename, module, require, exports){
     console.log(_dirname);
     console.log(_filename);
-    console.log(module);
-    console.log(require);
-    console.log(exports);
+    
+    const add = require('./calculator.js');
+    console.log(add(2, 3));
+
+    require('http');
+    const server = http.createServer(function(req, res){
+        res.writeHead(200);
+        res.end('Hello World!');
+    });
+
+    server.listen(8080);
 // }
