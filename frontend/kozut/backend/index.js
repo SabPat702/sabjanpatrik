c2884f7c6b32750517a3da3e32b6945c6388a2a8
Sabján Patrik <SabPat702@hengersor.hu>
2025-01-24 12:25:02 +0100

    Update index.js

--- frontend/kozut/backend/index.js
+++ frontend/kozut/backend/index.js
@@ -5,3 +5,18 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    password: "",
+    database: "kozutak",
+});
+
+app.get("/", (req, res) => {
+    res.send("Fut a backend.");
+});
+
+app.listen(3001, () =>{
+    console.log("Server is running on port 3001");
+});
