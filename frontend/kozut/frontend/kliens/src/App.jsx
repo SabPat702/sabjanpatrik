1ffe4104281e28e10ce27f4492f938b4b9b92ef2
Sabján Patrik <SabPat702@hengersor.hu>
2025-02-20 13:23:41 +0100

    kozut

--- frontend/kozut/frontend/kliens/src/App.jsx
+++ frontend/kozut/frontend/kliens/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'tachyons/css/tachyons.min.css'
 
 function App() {
-
-
+  const [felhasznalok, setFelhasznalok] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3001/regiok`)
+        .then(res => res.json())
+        .then(data => setFelhasznalok(data))
+  }, []);
   return (
-    <>
-      
-      <h1>Közutak</h1>
-      
-    </>
+      <div className="container-fluid bg-light-gray">
+            <article className="row justify-content-center ">
+                <h1 className="text-center display-4 mt-5 mb-5 bg-light-blue">Régiók</h1>
+                <div className="row">
+                        {felhasznalok.map(felhasznalo => (
+                            <div className="col-md-4">
+                                <div className=" bg-light-red br3 pa3 ma2 grow bw2 shadow-5" key={felhasznalo.Rid}>
+                                    <h2 className="text-center">Régió:</h2>
+                                    <p>Régió neve: {felhasznalo.regionev}</p>
+                                    <p>Régió típusa: {felhasznalo.regio_tipusa}</p>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </article>
+        </div>
   )
 }
 
