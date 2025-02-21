a79723d5ed0593b702ed6fa176cc6ce7130e85b8
Sabján Patrik <SabPat702@hengersor.hu>
2025-02-21 11:27:54 +0100

    kuzotak tablas

--- frontend/kozut/frontend/kliens/src/App.jsx
+++ frontend/kozut/frontend/kliens/src/App.jsx
@@ -2,30 +2,44 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'tachyons/css/tachyons.min.css'
+import axios from 'axios'
 
 function App() {
-  const [felhasznalok, setFelhasznalok] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    fetch(`http://localhost:3001/regiok`)
-        .then(res => res.json())
-        .then(data => setFelhasznalok(data))
+    axios.get(`http://localhost:3001/regiok`)
+        .then(data => setData(data.data))
+        .catch(error => console.log("Hiba:" + error))
   }, []);
   return (
-      <div className="container-fluid bg-light-gray">
-            <article className="row justify-content-center ">
-                <h1 className="text-center display-4 mt-5 mb-5 bg-light-blue">Régiók</h1>
-                <div className="row">
-                        {felhasznalok.map(felhasznalo => (
-                            <div className="col-md-4">
-                                <div className=" bg-light-red br3 pa3 ma2 grow bw2 shadow-5" key={felhasznalo.Rid}>
-                                    <h2 className="text-center">Régió:</h2>
-                                    <p>Régió neve: {felhasznalo.regionev}</p>
-                                    <p>Régió típusa: {felhasznalo.regio_tipusa}</p>
-                                </div>
-                            </div>
-                        ))}
-                </div>
-            </article>
+        <div className="container-fluid">
+            <h1 className="text-center display-4 mt-5 mb-5">Régiók</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rid</th>
+                        <th>Régió</th>
+                        <th>Régió típusa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {data.map(row => (
+                    <tr key={row.Rid}>
+                        <td>{row.Rid}</td>
+                        <td>{row.regionev}</td>
+                        <td>{row.regio_tipusa}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <div>
+                <select name='regioSelect' id='regioSelector'>
+                    {data.map(d => (
+                        <option key={d.Rid} value={d.regionev}>{d.regionev}</option>
+                    ))}
+                </select>
+                <button onClick='yes' type='submit'>Beküld</button>
+            </div>
         </div>
   )
 }
