be06d44903c8219d95a4876b93a0e55a24f95eca
SabPat702 <SabPat702@hengersor.hu>
2025-02-28 16:40:48 +0100

    no seperate main menu :(

--- mestermunka/orai/Asztali alkalmazas/Dungeon_Valley_Explorer/MainMenu.cs
+++ mestermunka/orai/Asztali alkalmazas/Dungeon_Valley_Explorer/MainMenu.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Dungeon_Valley_Explorer
 {
     static class MainMenu
     {
-
+        
     }
 }
